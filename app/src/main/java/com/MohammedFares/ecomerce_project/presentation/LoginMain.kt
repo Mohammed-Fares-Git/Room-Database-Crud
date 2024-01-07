42585d3b71a3ed3e361b411f5b755ecdfaec9af6
Mohammed-Fares-Git <mohammedfaresgit@gmail.com>
2024-01-07 16:31:46 +0100

    make LogonMain the launcher activity

--- app/src/main/java/com/MohammedFares/ecomerce_project/presentation/LoginMain.kt
+++ app/src/main/java/com/MohammedFares/ecomerce_project/presentation/LoginMain.kt
@@ -1,14 +1,36 @@
 package com.MohammedFares.ecomerce_project.presentation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.MohammedFares.ecomerce_project.R
+import com.MohammedFares.ecomerce_project.auth.AuthManager
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class LoginMain : AppCompatActivity() {
+
+    private lateinit var authManager: AuthManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_main)
+
+
+        authManager = AuthManager(this)
+
+        val isClientLoggedIn = authManager.isLoggedIn && authManager.isClientLoggedIn()
+        val isAdminLoggedIn = authManager.isLoggedIn && authManager.isAdminLoggedIn()
+
+
+        if (isClientLoggedIn) {
+            startActivity(Intent(this@LoginMain,ClientMian::class.java))
+            finish()
+        } else if (isAdminLoggedIn) {
+            Intent(this@LoginMain,ClientMian::class.java).also {
+                startActivity(it)
+                finish()
+            }
+        }
     }
+
 }
